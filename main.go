3a9c6e796f686b1a0a8ac7887b9f6e195c53e4b3
SpielerEinzig <SpielerEinzig@gmail.com>
2023-01-19 08:20:33 +0100

    declaring variables in go

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func main () {
+
+	//strings
+	var greet string = "Hello friend"
+	var name = "User"
+	var second string
+	third := "Meh"
+
+	second = "Mister"
+
+	fmt.Println(greet, name, second, third)
+
+	//integers
+	var age1 int = 1000000000
+	var age2  = 20
+	age3 := 30
+
+	fmt.Println(age1, age2, age3)
+
+
+	//decimals
+	var d1 float64 = 0.42
+	var d2 float64 = 329838.52
+	d3 := 288.43
+
+	fmt.Println(d1, d2, d3)
+} 
