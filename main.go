8bb7994c55fedbcc57b56d8a4d593e585c8fc63d
SpielerEinzig <SpielerEinzig@gmail.com>
2023-01-19 09:25:28 +0100

    arrayRangeSlice

--- main.go
+++ main.go
@@ -3,30 +3,30 @@ package main
 import "fmt"
 
 func main() {
-	////1: Arrays
-	////arrays with fixed length
-	// var ages [3]int = [3]int {1, 2, 3}
-	// fmt.Println(ages)
+	//1: Arrays
+	//arrays with fixed length
+	var ages [3]int = [3]int {1, 2, 3}
+	fmt.Println(ages)
 
-	////short hand
-	// ageList := [3]int {3, 2, 1}
-	// fmt.Println(ageList)
+	//short hand
+	ageList := [3]int {3, 2, 1}
+	fmt.Println(ageList)
 
-	////print out length of array
-	// fmt.Println("'ages' array is ", len(ages), "long")
-	// fmt.Println("'ageList' array is ", len(ageList), "long")
+	//print out length of array
+	fmt.Println("'ages' array is ", len(ages), "long")
+	fmt.Println("'ageList' array is ", len(ageList), "long")
 
 
 	//2: Slices
 	//slices are basically an array without a fixed length
-	// var slice = []int{5, 6, 7, 8}
-	// fmt.Println(slice)
-	// //change item at an index (works for arrays as well)
-	// slice[2] = 12
-	// fmt.Println(slice)
-	// //add item to the slice (works for array as well)
-	// slice = append(slice, 56)
-	// fmt.Println(slice)
+	var slice = []int{5, 6, 7, 8}
+	fmt.Println(slice)
+	//change item at an index (works for arrays as well)
+	slice[2] = 12
+	fmt.Println(slice)
+	//add item to the slice (works for array as well)
+	slice = append(slice, 56)
+	fmt.Println(slice)
 
 
 	//3. Ranges
