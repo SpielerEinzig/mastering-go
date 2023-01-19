8fcc345077b74a23d9a841239cb69d848752c083
SpielerEinzig <SpielerEinzig@gmail.com>
2023-01-19 09:23:56 +0100

    worked with arrays, slices and ranges

--- main.go
+++ main.go
@@ -1,27 +1,41 @@
 package main
 
-import (
-	"fmt")
+import "fmt"
 
-func main () {
+func main() {
+	////1: Arrays
+	////arrays with fixed length
+	// var ages [3]int = [3]int {1, 2, 3}
+	// fmt.Println(ages)
 
-	var name string = "Ryan"
-	var age = 1000
+	////short hand
+	// ageList := [3]int {3, 2, 1}
+	// fmt.Println(ageList)
 
-	//print line prints in separate lines as
-	//opposed to the print
-	fmt.Println(name, age)
+	////print out length of array
+	// fmt.Println("'ages' array is ", len(ages), "long")
+	// fmt.Println("'ageList' array is ", len(ageList), "long")
 
-	//print
-	fmt.Print(name)
-	fmt.Print(age)
 
-	//formatting strings
-	//print f is used to print formatted outputs to the console
-	//sprint f saves formatted outputs to a variable
-	//%v for varables, %q to quote the variable (only works on strings)
-	//%T for the type of variable
-	fmt.Printf("Hello, my name is %q and I am %v years old", name, age)
-	var data = fmt.Sprintf("Hello, my name is %q and I am %v years old", name, age)
-	fmt.Println(data)
+	//2: Slices
+	//slices are basically an array without a fixed length
+	// var slice = []int{5, 6, 7, 8}
+	// fmt.Println(slice)
+	// //change item at an index (works for arrays as well)
+	// slice[2] = 12
+	// fmt.Println(slice)
+	// //add item to the slice (works for array as well)
+	// slice = append(slice, 56)
+	// fmt.Println(slice)
+
+
+	//3. Ranges
+	//Basically getting a range from one array
+	//for example index 2 to 5 within an array or slice
+	//Also works for slices
+
+	var nameList = []string {"Esdeath", "Revy", "Balalaika"}
+	var rangeList = nameList[1:]
+	fmt.Println(rangeList)
+
 } 
